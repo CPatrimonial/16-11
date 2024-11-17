c76f0af256e79b8256111c76e88267d81ee17921
CPatrimonial <consultoria@creditopatrimonial.com.br>
2024-11-17 10:18:25 -0300

    2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    domains: [], // Adicione aqui os domínios das imagens externas se necessário
+    unoptimized: false,
+  },
 };
 
 module.exports = nextConfig;
